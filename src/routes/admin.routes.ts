ce561f719820e5196c7078b9b039cd9f1106313e
Yash Lodaya <iyashlodaya@gmail.com>
2025-01-17 01:06:37 +0530

    feat: added auth and admin routes.

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 
+import { addItem } from '../controllers/admin/add.item';
+import { getAllItems } from '../controllers/admin/get.all.items';
+
 const router: Router = Router();
 
 // Admin routes
-router.get('/', () => {
+router.get('/test', () => {
     console.log('Admin Test API.')
 });
+
+router.post('/groceries', addItem)
+router.get('/groceries', getAllItems)
 
 export default router;
