221c2d74514cab6b69809773b2f9600e88178e2c
Yash Lodaya <iyashlodaya@gmail.com>
2025-01-18 18:28:55 +0530

    feat: added user routes for viewing grocery items list, creating order and viewing order.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import verifyJWT from '../middleware/verify.token';
+import { getGroceryItemList } from '../controllers/user/get.grocery.item.list';
+import { createOrder } from '../controllers/user/create.order';
+import { viewUserOrders } from '../controllers/user/view.order';
 
 const router: Router = Router();
 
-// Admin routes
-router.get('/', () => {
+// User routes
+router.get('/test', () => {
     console.log('User Test API.')
 });
+
+router.post('/grocery-list', verifyJWT, getGroceryItemList)
+
+//order routes
+router.post('/orders', verifyJWT, createOrder)
+router.get('/orders', verifyJWT, viewUserOrders)
 
 export default router;
