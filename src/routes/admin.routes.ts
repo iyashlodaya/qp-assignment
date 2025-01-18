d8e7bf66ca4b1ba2af6cabe0dc365d0893b567ac
Yash Lodaya <iyashlodaya@gmail.com>
2025-01-18 17:04:22 +0530

    feat: added remaining routes for admin apis.

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,16 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { addItem } from '../controllers/admin/add.item';
-import { getAllItems } from '../controllers/admin/get.all.items';
+import { addItem } from "../controllers/admin/add.item";
+import { getAllItems } from "../controllers/admin/get.all.items";
+import { deleteItem } from "../controllers/admin/delete.item";
+import { updateItem } from "../controllers/admin/update.item";
+import { updateStock } from "../controllers/admin/update.stock";
+
+//middleware
+import verifyJWT from "../middleware/verify.token";
+
 
 const router: Router = Router();
 
 // Admin routes
-router.get('/test', () => {
-    console.log('Admin Test API.')
+router.get("/test", () => {
+  console.log("Admin Test API.");
 });
 
-router.post('/groceries', addItem)
-router.get('/groceries', getAllItems)
+router.post("/groceries", verifyJWT, addItem);
+router.get("/groceries/:category?", verifyJWT, getAllItems);
+router.delete("/groceries/:id?", verifyJWT, deleteItem);
+router.put("/groceries/:id?", updateItem);
+router.patch("/groceries/:id?/stock", updateStock);
 
 export default router;
