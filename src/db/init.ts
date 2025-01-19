5b0417c754f64997f0a8f59813881ea45be1b910
Yash Lodaya <iyashlodaya@gmail.com>
2025-01-19 17:06:11 +0530

    fix: optmized the db init script, now to create db if not exists.

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,12 +1,30 @@
 import { User, Item, Order, OrderItemMapping } from "./models";
 import '../db/models/associations'; // This will apply the associations
+import sequelize from "./config";
+import { QueryTypes } from "sequelize";
 
 const dbInit = async () => {
   try {
+
+    const dbExists = await sequelize.query(
+      "SELECT * from pg_database where datname = :dbname",
+      {
+        replacements: {dbname: process.env.DB_NAME},
+        type: QueryTypes.SELECT,
+      }
+    );
+
+    if(!dbExists.length) {
+      await sequelize.query(`CREATE DATABASE ${process.env.DB_NAME}`);
+      console.log(`Database ${process.env.DB_NAME} created successfully!`);
+    }
+
+    await Order.sync({ alter: true });
+
+
     await Promise.all([
         User.sync({ alter: true }),
         Item.sync({ alter: true }),
-        Order.sync({ alter: true }),
         OrderItemMapping.sync({ alter: true }),
     ]);
     console.log("Database Synchronization Completed!");
